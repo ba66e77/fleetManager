56ccf85d6f99f192aee6c12e9b93356688da62e8
Barrett Smith <barrett@datasmith.net>
2024-08-11 19:18:06 -0400

    Source .env file if it exists, to help with motherduck authentication.

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -1,1 +1,11 @@
+if [ -f '.env' ]; then
+    set -a
+    source '.env'
+    set +a
+    echo "Read .env file"
+else
+    echo "No .env file found"
+fi
+
 duckdb -s ".read 'db_setup.sql'"
+echo "Pipeline run complete"
